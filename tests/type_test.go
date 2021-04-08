95a16708a4769527feebabc642c8d05b25dee429
wu452148993 <38494302+wu452148993@users.noreply.github.com>
2021-04-09 02:39:59 +0800

    Add more sub-selector fun, optimize storage structure (#7)
    * Add more sub-selector fun, optimize storage structure
    
    * Optimize sub-selector fun name
    
    * Add function comment

--- tests/type_test.go
+++ tests/type_test.go
@@ -7,50 +7,26 @@ import (
 	"testing"
 )
 
-func TestBase(t *testing.T) {
+func TestType(t *testing.T) {
 	tests := []struct {
 		name   string
 		input  string
 		output string
 	}{
 		{
-			name: "Read",
-			input: TestMain(`x := m.Read("cell1", 0)
-print(x)`, true, false),
-			output: `read _main_x cell1 0
-print _main_x`,
-		},
-		{
-			name:   "Write",
-			input:  TestMain(`m.Write(1, "cell1", 0)`, true, false),
-			output: `write 1 cell1 0`,
-		},
-		{
-			name:   "PrintFlush",
-			input:  TestMain(`m.PrintFlush("message1")`, true, false),
-			output: `printflush message1`,
-		},
-		{
-			name: "GetLink",
-			input: TestMain(`x := m.GetLink(0)
-print(x)`, true, false),
-			output: `getlink _main_x 0
-print _main_x`,
-		},
-		{
-			name: "Radar",
+			name: "Radar_This",
 			input: TestMain(`x := m.Radar(m.This, m.RTAlly, m.RTEnemy, m.RTBoss, false, m.RSArmor)
-print(x)`, true, false),
+				print(x)`, true, false),
 			output: `radar ally enemy boss armor @this false _main_x
 print _main_x`,
 		},
 		{
-			name: "Sensor",
+			name: "Sensor_GetHealth",
 			input: TestMain(`_, _, _, b := m.UnitLocateDamaged()
-x := m.Sensor(b, "B")
+x := b.GetHealth()
 print(x)`, true, false),
 			output: `ulocate damaged core true @copper @_ @_ @_ _main_b
-sensor _main_x _main_b B
+sensor _main_x _main_b @health
 print _main_x`,
 		},
 	}
@@ -64,6 +40,7 @@ print _main_x`,
 				t.Error(err)
 				return
 			}
+
 			test.output = test.output + "\nend"
 			assert.Equal(t, test.output, strings.Trim(mlog, "\n"))
 		})
